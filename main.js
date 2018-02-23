f038e429c5dd4eb2d48f2604c13833270a178e40
kawer <kawer7@gmail.com>
2018-02-23 12:54:29 -0600

    added text

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 
-var textel = document.querySelector("#text");
+var wordel = document.querySelector("#word");
+var completeText = document.querySelector("#completeText");
 
 var data = `When an object is initialized with another object of the same type, the new object is said to be a copy of the initializing object, even if the copy was created via the move constructor. Regrettably, there’s no terminology in C++ that distinguishes between an object that’s a copy-constructed copy and one that’s a move-constructed copy`
+
+completeText.textContent = data;
 
 function reader(toRead) {
     let ind = 0;
     let wordList = toRead.split(" ");
     return function () {
-        textel.textContent = wordList[ind++];
+        wordel.textContent = wordList[ind++];
     }
 }
 
